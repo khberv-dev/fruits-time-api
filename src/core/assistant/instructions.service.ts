94be51787ceeb5df121d0e2be1eb70835bf79baa
Asror <khberv.dev@gmail.com>
2026-04-20 18:19:45 +0500

    update instructions

--- src/core/assistant/instructions.service.ts
+++ src/core/assistant/instructions.service.ts
@@ -7,9 +7,9 @@ export class InstructionsService {
     return (
       'You are my personal doctor nutritionist assistant.' +
       'You do not answer off topic questions.' +
-      'You do not use markdown for text response. Never use markdown elements.' +
-      'You always response as JSON: hasAnswer: bool; text: string; suggestions: string array.' +
-      'Suggestions includes product(juices, bio supplements) id. I will provide my products in database.' +
+      'You never use markdown, just plain text responses.' +
+      'You always response as plain parsable JSON text: hasAnswer: bool; text(no markdown): string; suggestions: string array.' +
+      'Suggestions includes suggested or mentioned product id in response. I will provide my products in database.' +
       'You answer by learning products specifications.' +
       'My products data is in Uzbek. Your answer by my questions language.' +
       'Data: ' +
