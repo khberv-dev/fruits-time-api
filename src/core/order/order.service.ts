6f52ef68904e249c286393cd7af6aec977b07ab1
Asror <khberv.dev@gmail.com>
2026-05-29 15:37:36 +0500

    update delivery body

--- src/core/order/order.service.ts
+++ src/core/order/order.service.ts
@@ -18,7 +18,7 @@ function applyDiscount(price: number, discountPercent: number): number {
   return Math.round(price * (1 - discountPercent / 100));
 }
 
-const POSTER_DELIVERY = { courierId: 1, deliveryPrice: 15000, processingStatus: 30 };
+const POSTER_DELIVERY = { courierId: 1, deliveryPrice: 15000, processingStatus: 40 };
 
 @Injectable()
 export class OrderService {
